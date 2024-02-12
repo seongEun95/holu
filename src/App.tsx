d0fac1202ae93cd8a6697483b78a23659ff9f03f
seong eun <17adm@naver.com>
2024-02-12 21:12:18 +0900

    초기세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p css={testCss}>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
+
+const testCss = css`
+	color: red;
+`;
 
 export default App;
