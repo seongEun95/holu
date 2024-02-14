f938a436124c86385eb2880d7b32e54e7791d09d
seong eun <17adm@naver.com>
2024-02-14 22:10:42 +0900

    아바타 컴포넌트 테스트 페이지

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,7 +1,72 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
+import Avatar from '../components/ui/Avatar';
 
 export default function TestPage() {
-	return <div>test page</div>;
+	return (
+		<div css={testCss}>
+			<div>
+				<div css={testTitleCss}>iconOnly | medium</div>
+				<Avatar
+					src="https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG"
+					alt="icon"
+					name="AvatarName"
+					size="medium"
+					type="iconOnly"
+				/>
+			</div>
+
+			<div>
+				<div css={testTitleCss}>iconWithName | small</div>
+				<Avatar
+					src="https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG"
+					alt="icon"
+					name="AvatarName"
+					size="small"
+					type="iconWithName"
+				/>
+			</div>
+
+			<div>
+				<div css={testTitleCss}>iconWithNameAndDate | large</div>
+				<Avatar
+					src="https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG"
+					alt="icon"
+					name="AvatarName"
+					size="large"
+					type="iconWithNameAndDate"
+					date={new Date()}
+				/>
+			</div>
+
+			<div>
+				<div css={testTitleCss}>iconWithNameAndDateTwoLines | large</div>
+				<Avatar
+					src="https://hola-post-image.s3.ap-northeast-2.amazonaws.com/default.PNG"
+					alt="icon"
+					name="AvatarName"
+					size="large"
+					type="iconWithNameAndDateTwoLines"
+					date={new Date()}
+				/>
+			</div>
+		</div>
+	);
 }
+
+const testCss = css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 30px;
+	width: 100%;
+	height: 100vh;
+	background: #ededed;
+`;
+
+const testTitleCss = css`
+	font-size: 16px;
+	color: #183e2a;
+	margin-bottom: 20px;
+`;
