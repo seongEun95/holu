308fd4f55a037a2dd2197286e9175f6d9a5416ac
seong eun <17adm@naver.com>
2024-02-26 01:08:09 +0900

    등록페이지 데이터

---
+++ src/data/registerData.ts
@@ -0,0 +1,89 @@
+export const SelectItems = [
+	{
+		label: '프로젝트',
+		value: 'project',
+	},
+	{
+		label: '스터디',
+		value: 'study',
+	},
+	{
+		label: '기타',
+		value: 'etc',
+	},
+];
+
+export const peopleNumbers = [
+	{
+		label: '인원미정',
+		value: 'N',
+	},
+	{
+		label: '1명',
+		value: '1',
+	},
+	{
+		label: '2명',
+		value: '2',
+	},
+];
+
+export const process = [
+	{
+		label: '전체',
+		value: 'all',
+	},
+	{
+		label: '온라인',
+		value: 'online',
+	},
+	{
+		label: '오프라인',
+		value: 'offline',
+	},
+];
+
+export const period = [
+	{
+		label: '기간 미정',
+		value: 'no period',
+	},
+	{
+		label: '1개월',
+		value: 'one',
+	},
+	{
+		label: '2개월',
+		value: 'two',
+	},
+];
+
+export const skill = [
+	{
+		label: 'javaScript',
+		value: 'javaScript',
+	},
+	{
+		label: 'React',
+		value: 'React',
+	},
+	{
+		label: 'TypeScript',
+		value: 'TypeScript',
+	},
+];
+
+export const position = [
+	{
+		label: '전체',
+		value: 'all',
+	},
+	{
+		label: '프론트엔드',
+		value: 'frontend',
+	},
+	{
+		label: '벡엔드',
+		value: 'backend',
+	},
+];
