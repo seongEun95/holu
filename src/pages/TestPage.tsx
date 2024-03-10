3b40db2f7306cb80a9bb36e6076075145f25cb8c
seong eun <17adm@naver.com>
2024-03-10 10:19:48 +0900

    드랍다운, 조회수 컴포넌트 테스트

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,34 +1,34 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import Button from '../components/ui/Button';
+import DropdownMenu from '../components/ui/DropdownMenu';
+import { OPTIONS_LANGUAGE } from '../data/mainDropdownData';
+import UserCountIcon from '../components/ui/UserCountIcon';
+import { useState } from 'react';
 
 export default function TestPage() {
+	const [value, setValue] = useState({
+		language: '',
+	});
+
+	const handleClickGetOption = (e: any) => {
+		const { name, value } = e.target;
+		console.log(name, value);
+		setValue(prev => ({ ...prev, [name]: value }));
+	};
+
 	return (
 		<div css={testCss}>
-			<Button kind="submit" size="medium">
-				확인
-			</Button>
-
-			<Button kind="comment" size="large">
-				댓글 달기
-			</Button>
-
-			<Button kind="comment" size="medium">
-				댓글 달기
-			</Button>
-
-			<Button kind="comment" size="small">
-				댓글 달기
-			</Button>
-
-			<Button kind="cancel" size="medium">
-				취소
-			</Button>
-
-			<Button kind="normal" size="medium">
-				확인
-			</Button>
+			<DropdownMenu
+				name="language"
+				placeholder="언어"
+				options={OPTIONS_LANGUAGE}
+				value={value.language}
+				onClick={handleClickGetOption}
+			/>
+			<UserCountIcon type="view" count={1} />
+			<UserCountIcon type="bookmark" count={71} />
+			<UserCountIcon type="comment" count={1} />
 		</div>
 	);
 }
