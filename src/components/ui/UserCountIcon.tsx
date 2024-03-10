d64c1afff13e67665af2ed687cd4e0ddc4fb0c12
seong eun <17adm@naver.com>
2024-03-10 10:17:01 +0900

    조회수, 북마크, 댓글 수 컴포넌트

---
+++ src/components/ui/UserCountIcon.tsx
@@ -0,0 +1,45 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+import { GrView } from 'react-icons/gr';
+import { PiHandWavingLight } from 'react-icons/pi';
+import { IoChatbubbleOutline } from 'react-icons/io5';
+
+export type IconType = 'view' | 'bookmark' | 'comment';
+
+type UserCountIconProps = {
+	type: string;
+	count: number;
+};
+
+const renderIcon = (type: string) => {
+	switch (type) {
+		case 'view':
+			return <GrView size={20} color="#999" />;
+		case 'bookmark':
+			return <PiHandWavingLight size={25} color="#999" />;
+		case 'comment':
+			return <IoChatbubbleOutline size={20} color="#999" />;
+	}
+};
+
+export default function UserCountIcon({ type, count }: UserCountIconProps) {
+	return (
+		<div css={iconWrapCss}>
+			<span>{renderIcon(type)}</span>
+			<span css={countCss}>{count}</span>
+		</div>
+	);
+}
+
+const iconWrapCss = css`
+	display: flex;
+	align-items: center;
+	gap: 8px;
+`;
+
+const countCss = css`
+	font-size: 14px;
+	font-weight: 500;
+	color: #999;
+`;
