0a91e763c775b21e46944f2f29810dc20293602a
seong eun <17adm@naver.com>
2024-03-12 00:10:56 +0900

    드랍다운 데이터

---
+++ src/data/mainDropdownData.ts
@@ -0,0 +1,44 @@
+export const OPTIONS_LANGUAGE = [
+	{
+		label: '리액트',
+		value: 'react',
+	},
+	{
+		label: '넥스트',
+		value: 'nextjs',
+	},
+	{
+		label: '자바',
+		value: 'java',
+	},
+];
+
+export const OPTIONS_POSITION = [
+	{
+		label: '전체',
+		value: 'all',
+	},
+	{
+		label: '프론트엔드',
+		value: 'frontend',
+	},
+	{
+		label: '벡엔드',
+		value: 'backend',
+	},
+];
+
+export const OPTIONS_METHOD = [
+	{
+		label: '전체',
+		value: 'all',
+	},
+	{
+		label: '온라인',
+		value: 'online',
+	},
+	{
+		label: '오프라인',
+		value: 'offline',
+	},
+];
