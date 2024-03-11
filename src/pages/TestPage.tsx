7b1fae7cc1fd3024f771c0ca5aace7e658f0b542
seong eun <17adm@naver.com>
2024-03-12 00:09:43 +0900

    카드 컴포넌트 테스트

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,10 +1,11 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
+import { useState } from 'react';
+import Card from '../components/ui/Card';
 import DropdownMenu from '../components/ui/DropdownMenu';
 import { OPTIONS_LANGUAGE } from '../data/mainDropdownData';
-import UserCountIcon from '../components/ui/UserCountIcon';
-import { useState } from 'react';
+import dayjs from 'dayjs';
 
 export default function TestPage() {
 	const [value, setValue] = useState({
@@ -19,16 +20,24 @@ export default function TestPage() {
 
 	return (
 		<div css={testCss}>
+			<Card
+				category="PROJECT"
+				uploadDate={dayjs('2024-03-07')}
+				deadline={dayjs('2024-03-14')}
+				projectTitle="포트폴리오용 자취생 커뮤니티 프론트 구함"
+				position={['프론트엔드', 'IOS']}
+				skillStack={['aws', 'c']}
+				userId="우멍루"
+				viewCount={16}
+				commentCount={4}
+			/>
 			<DropdownMenu
 				name="language"
 				placeholder="언어"
-				options={OPTIONS_LANGUAGE}
 				value={value.language}
+				options={OPTIONS_LANGUAGE}
 				onClick={handleClickGetOption}
 			/>
-			<UserCountIcon type="view" count={1} />
-			<UserCountIcon type="bookmark" count={71} />
-			<UserCountIcon type="comment" count={1} />
 		</div>
 	);
 }
