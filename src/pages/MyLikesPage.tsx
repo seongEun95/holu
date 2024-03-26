40d99c52a2a31be63c56ff0d5942c9008686ee7b
seong eun <17adm@naver.com>
2024-03-26 22:42:29 +0900

    내 관심글 페이지 추가

--- src/pages/MyLikesPage.tsx
+++ src/pages/MyLikesPage.tsx
@@ -6,21 +6,27 @@ import { RootState } from '../redux/store';
 import { Bookmark } from '../redux/slice/bookmarkSlice';
 import Card from '../components/ui/Card';
 import mq from '../styles/mediaQuery';
+import { CgTrashEmpty } from 'react-icons/cg';
 
 export default function MyLikesPage() {
 	const bookmarkItems = useSelector((state: RootState) => state.bookmark.items);
 
 	return (
-		<div css={bookmarkWrapCss}>
-			{bookmarkItems.length === 0
-				? '북마크 없음'
-				: bookmarkItems.map((item: Bookmark) => <Card key={item.id} {...item} />)}
+		<div css={bookmarkWrapCss(bookmarkItems.length === 0)}>
+			{bookmarkItems.length === 0 ? (
+				<div css={emptyBookmarkCss}>
+					<CgTrashEmpty size={40} />
+					<div>내 관심글 없음</div>
+				</div>
+			) : (
+				bookmarkItems.map((item: Bookmark) => <Card key={item.id} {...item} />)
+			)}
 		</div>
 	);
 }
 
-const bookmarkWrapCss = css`
-	display: grid;
+const bookmarkWrapCss = (isBookmark: boolean) => css`
+	display: ${isBookmark ? 'block' : 'grid'};
 	grid-template-columns: repeat(4, 1fr);
 	gap: 20px;
 	width: 100%;
@@ -40,4 +46,14 @@ const bookmarkWrapCss = css`
 	${mq.tablet} {
 		grid-template-columns: repeat(1, 1fr);
 	}
+`;
+
+const emptyBookmarkCss = css`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 20px;
+	padding: 20px 0;
+	border: 1px solid #ccc;
+	border-radius: 20px;
 `;
